7ff851ebfffd9958adef8daa72421a540fc9028f
rizkysiregar <regarrezky@gmail.com>
2023-05-23 16:05:41 +0700

    string localization

--- app/src/main/java/com/rizkysiregar/ecommerce/ui/login/LoginActivity.kt
+++ app/src/main/java/com/rizkysiregar/ecommerce/ui/login/LoginActivity.kt
@@ -1,10 +1,14 @@
 package com.rizkysiregar.ecommerce.ui.login
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.Html
+import android.text.Spannable
+import android.text.SpannableString
 import android.text.TextWatcher
+import android.text.style.ForegroundColorSpan
 import android.util.Patterns
 import android.view.View
 import android.widget.Toast
@@ -28,6 +32,7 @@ class LoginActivity : AppCompatActivity() {
 
         checkInput()
         coloredText()
+//        spannableTextColor()
 
         // event to mainActivity
         val btnLogin = binding.btnMasukLogin
@@ -127,5 +132,33 @@ class LoginActivity : AppCompatActivity() {
     private fun coloredText() {
         val coloredText = getString(R.string.colored_text)
         binding.tvTermsLogin.text = Html.fromHtml(coloredText, Html.FROM_HTML_MODE_LEGACY)
+    }
+
+    private fun spannableTextColor() {
+        val text1 = getString(R.string.terms1)
+        val text3 = getString(R.string.terms3)
+        val text5 = getString(R.string.terms5)
+
+        val text2 = getString(R.string.terms2)
+        val text4 = getString(R.string.terms4)
+        val spannableString1 = SpannableString(text2)
+        val spannableString2 = SpannableString(text4)
+
+
+        spannableString1.setSpan(
+            ForegroundColorSpan(Color.MAGENTA),
+            0,
+            spannableString1.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        spannableString2.setSpan(
+            ForegroundColorSpan(Color.MAGENTA),
+            0,
+            spannableString2.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        binding.tvTermsLogin.text = "$text1 $spannableString1 $text3 $spannableString2 $text5"
     }
 }
